9067eaddc8f044e864f6381c8ff14d8745c7fb33
Ihar Hrachyshka <ihrachys@redhat.com>
2017-06-16 00:25:30 -0700

    Added missing test files
    Those were excluded from git before by mistake.

---
+++ cryptotrade/tests/test_config.py
@@ -0,0 +1,57 @@
+# Copyright 2017 Ihar Hrachyshka <ihar.hrachyshka@gmail.com>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+import os
+import tempfile
+import unittest
+
+from cryptotrade import config
+
+
+class TestGetConfig(unittest.TestCase):
+
+    def setUp(self):
+        super(TestGetConfig, self).setUp()
+        self.fp, self.fname = tempfile.mkstemp()
+        os.close(self.fp)
+
+    def tearDown(self):
+        os.remove(self.fname)
+        super(TestGetConfig, self).tearDown()
+
+    def test_empty_file(self):
+        self.assertEqual({}, config.get_config(self.fname))
+
+    def test_file_with_ini_contents(self):
+        contents = (
+            '[core]\n'
+            'opt0 = val0\n'
+            '[poloniex]\n'
+            'opt1 = val1\n'
+            'opt2 = val2\n'
+            '[unknown]\n'
+            'opt3 = val3\n'
+        )
+        with open(self.fname, 'w') as f:
+            f.write(contents)
+        res = config.get_config(self.fname)
+        self.assertIn('core', res)
+        self.assertIn('poloniex', res)
+        self.assertNotIn('unknown', res)
---
+++ cryptotrade/tests/test_exchange.py
@@ -0,0 +1,68 @@
+# Copyright 2017 Ihar Hrachyshka <ihar.hrachyshka@gmail.com>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+import unittest
+
+from cryptotrade import exchange
+
+
+class TestGetWorth(unittest.TestCase):
+
+    class FakeExchange(exchange.Exchange):
+
+        def get_balances(self):
+            return {'BTC': 5, 'XMR': 10, 'ETH': 100}
+
+        def get_rate(self, from_, to_):
+            if from_ == 'BTC':
+                if to_ == 'BTC':
+                    return 1
+                elif to_ == 'XMR':
+                    return 0.1
+                else:
+                    return 0.01
+            else:  # BTC -> USD
+                return 2500
+
+        # needed to fulfill abstract interface
+        def cancel_all_orders(self):
+            return NotImplemented
+
+        # needed to fulfill abstract interface
+        def get_candlesticks(self, from_, to_, period, start, end):
+            return NotImplemented
+
+    def setUp(self):
+        super(TestGetWorth, self).setUp()
+        config = object()
+        self.exchange = self.FakeExchange(config)
+
+    def test_get_worth_btc(self):
+        res = self.exchange.get_worth('BTC')
+        self.assertEqual(7, res)
+
+    def test_get_worth_usd(self):
+        res = self.exchange.get_worth('USD')
+        self.assertEqual(7 * 2500, res)
+
+    def test_get_worth_custom_balances(self):
+        balances = {'BTC': 10, 'ETH': 200, 'XMR': 30}
+        res = self.exchange.get_worth('BTC', balances=balances)
+        self.assertEqual(15, res)
