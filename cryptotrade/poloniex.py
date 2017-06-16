e208140c6b6496ce6a420403d9af4062ff8fc399
Ihar Hrachyshka <ihrachys@redhat.com>
2017-06-15 21:26:49 -0700

    Added poloniex module to interact with Poloniex exchange
    The initial version includes cancel_all_orders method only. The module
    implements the generic 'exchange' module that provides an abstract class
    to describe different exchanges.

---
+++ cryptotrade/poloniex.py
@@ -0,0 +1,36 @@
+# Copyright 2017 Ihar Hrachyshka <ihar.hrachyshka@gmail.com>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+import poloniex as plx
+
+from cryptotrade import exchange
+
+
+class Poloniex(exchange.Exchange):
+
+    def __init__(self, api_secret, api_key):
+        super().__init__()
+        self.public = plx.PoloniexPublic()
+        self.private = plx.Poloniex(apikey=api_key, secret=api_secret)
+
+    def cancel_all_orders(self):
+        for currency_pair, orders_ in self.private.returnOpenOrders().items():
+            for order in orders_:
+                self.private.cancelOrder(order['orderNumber'])
